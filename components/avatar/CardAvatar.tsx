5714e2fd584020204e50f6cf09f02f5be5eafa82
petrushandika <petrushandikasinaga@gmail.com>
2024-08-01 08:51:39 +0700

    feat: delete api function

--- components/avatar/CardAvatar.tsx
+++ components/avatar/CardAvatar.tsx
@@ -1,53 +1,27 @@
-import { useEffect, useState } from 'react';
-import { View, ScrollView, StyleSheet, TouchableOpacity, ActivityIndicator, Text } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { Avatar } from '@rneui/themed';
-import API from '@/networks/api';
+import datas from '@/data/data.json';
 
 const chunkArray = (arr: any[], chunkSize: number) => {
   if (chunkSize <= 0) throw "Invalid chunk size";
-  let result = [];
-  for (let i = 0; i < arr.length; i += chunkSize) {
-    result.push(arr.slice(i, i + chunkSize));
-  }
-  return result;
+  var R = [];
+  for (var i = 0; i < arr.length; i += chunkSize)
+    R.push(arr.slice(i, i + chunkSize));
+  return R;
 };
 
 const CardAvatar: React.FunctionComponent = () => {
   const [selectedAvatarId, setSelectedAvatarId] = useState<number | null>(null);
-  const [avatars, setAvatars] = useState<any[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
 
   const handleAvatarPress = (item: any) => {
     setSelectedAvatarId(item.id);
     console.log(`Avatar clicked: ${item.name}`);
   };
 
-  console.log("test");
-
-  useEffect(() => {
-    const fetchAvatars = async () => {
-      try {
-        const response = await API.AVATAR.GET_ALL();
-        console.log("ok", response.data);
-      } catch (err) {
-        setError('Failed to fetch avatars');
-        console.error(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchAvatars();
-  }, []);
-
-  if (loading) return <ActivityIndicator size="large" color="#0000ff" />;
-
-  if (error) return <Text style={styles.errorText}>{error}</Text>;
-
   return (
     <ScrollView style={styles.scrollView}>
-      {chunkArray(avatars, 4).map((chunk, chunkIndex) => (
+      {chunkArray(datas, 4).map((chunk, chunkIndex) => (
         <View
           style={styles.row}
           key={chunkIndex}
@@ -95,11 +69,6 @@ const styles = StyleSheet.create({
   selectedAvatar: {
     borderWidth: 2,
     borderColor: 'yellow',
-  },
-  errorText: {
-    color: 'red',
-    textAlign: 'center',
-    marginTop: 20,
   },
 });
 
